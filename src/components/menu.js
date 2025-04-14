05b321a2ea2f513994f70f50ac01507e9bbc76cc
anne <anne.gemma.lee@gmail.com>
2025-04-14 17:12:28 +0200

    get in touch section and footer added

---
+++ src/components/menu.js
@@ -0,0 +1,14 @@
+export const menu = [
+  {
+    name: "OUR COMPANY",
+    url: "/company",
+  },
+  {
+    name: "LOCATIONS",
+    url: "/locations",
+  },
+  {
+    name: "CONTACT",
+    url: "/contact",
+  },
+]
